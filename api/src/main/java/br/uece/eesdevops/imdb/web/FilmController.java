07580ce7da2e8c5e448582bfb297c2ea2369a36e
João Paulo de Farias <jpaulodefarias@gmail.com>
2020-05-21 23:51:12 -0300

    Added Film List Mapping

--- api/src/main/java/br/uece/eesdevops/imdb/web/FilmController.java
+++ api/src/main/java/br/uece/eesdevops/imdb/web/FilmController.java
@@ -2,6 +2,7 @@ package br.uece.eesdevops.imdb.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.util.List;
 
 import br.uece.eesdevops.imdb.domain.entity.Film;
 import br.uece.eesdevops.imdb.domain.service.FilmService;
@@ -17,18 +19,22 @@ import br.uece.eesdevops.imdb.domain.service.FilmService;
 @RequestMapping("/film")
 public class FilmController {
 
-       private final FilmService filmService;
+	private final FilmService filmService;
 
-    public FilmController(FilmService filmService) {
-        this.filmService = filmService;
+	public FilmController(FilmService filmService) {
+		this.filmService = filmService;
+	}
+
+	@GetMapping(produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Film>> getAll() {
+        List<Film> films = filmService.getAll();
+        return ResponseEntity.ok(films);
     }
 
-    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<Film> save(@RequestBody Film film) {
-        Film saved = filmService.execute(film);
-        return ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(saved);
-    }
-   
+	@PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+	public ResponseEntity<Film> save(@RequestBody Film film) {
+		Film saved = filmService.execute(film);
+		return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+	}
+
 }
