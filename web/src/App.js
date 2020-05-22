363f26a99a7fe1f1a4680e368306be26fd799835
João Paulo de Farias <jpaulodefarias@gmail.com>
2020-05-21 21:41:23 -0300

    Style update

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,13 @@
 import React, { Fragment } from 'react';
+import "./App.css";
 
 const App = props => {
 	return <Fragment>
-		<div class="uk-child-width-1-2@s" uk-grid>
-			<div>
-				<div class="uk-light uk-background-secondary uk-padding">
-					<h3>Light</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-					<button class="uk-button uk-button-default">Button</button>
-				</div>
-			</div>
-			<div>
-				<div class="uk-dark uk-background-muted uk-padding">
-					<h3>Dark</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-					<button class="uk-button uk-button-default">Button</button>
-				</div>
+		<div>
+			<div className="uk-light uk-background-secondary uk-padding">
+				<h2>Title</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				<button className="uk-button uk-button-default">Button</button>
 			</div>
 		</div>
 	</Fragment>;
