b73273f3ad764f0ae45eaad1e2710085950213c2
Ingrid Batista <fingridbatista@gmail.com>
2020-05-24 14:39:06 -0300

    Commit the class EvaluationService

--- api/src/main/java/br/uece/eesdevops/imdb/domain/service/EvaluationService.java
+++ api/src/main/java/br/uece/eesdevops/imdb/domain/service/EvaluationService.java
@@ -1,26 +1,19 @@
 package br.uece.eesdevops.imdb.domain.service;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.uece.eesdevops.imdb.domain.entity.Evaluation;
-import br.uece.eesdevops.imdb.domain.entity.Film;
-import br.uece.eesdevops.imdb.domain.exception.FilmInvalidException;
-import br.uece.eesdevops.imdb.domain.exception.FilmNotFoundException;
 import br.uece.eesdevops.imdb.repository.EvaluationRepository;
-import br.uece.eesdevops.imdb.repository.FilmRepository;
+
 
 @Service
 public class EvaluationService {
 
-    private final EvaluationRepository evaluationRepository;
-    //private final FilmRepository filmRepository;
+	private final EvaluationRepository evaluationRepository;
 
     public EvaluationService(EvaluationRepository evaluationRepository) {
         this.evaluationRepository = evaluationRepository;
-        //this.filmRepository = filmRepository;
     }
 
     @Transactional
@@ -29,20 +22,4 @@ public class EvaluationService {
         //System.out.println("O valor do titulo do film é " + evaluation.getFilm().getTitle());
         return evaluationRepository.save(evaluation);
     }
-
-    /*private void requireFilm(Evaluation evaluation) {
-        if (evaluation.getFilm() == null ) {
-			//|| evaluation.getFilm().getId() == null) {
-            throw new FilmInvalidException("Film ID is null");
-        }
-    }
-
-    private Film getFilmOrThrow(Integer filmId) {
-        final Optional<Film> optional = filmRepository.findById(filmId);
-        if (optional.isPresent()) {
-            return optional.get();
-        } else {
-            throw new FilmNotFoundException(filmId);
-        }
-    }*/
 }
