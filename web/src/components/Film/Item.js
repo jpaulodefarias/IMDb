baeabf6f73b02c6e08fd4f800682ca3b7650d78d
João Paulo de Farias <jpaulodefarias@gmail.com>
2020-05-28 01:41:52 -0300

    [Web] Adding Film Evaluations

--- web/src/components/Film/Item.js
+++ web/src/components/Film/Item.js
@@ -12,6 +12,27 @@ const Item = props => {
 				<p>Actors: {film.actors}</p>
 				<p>Producers: {film.producers}</p>
 				<p>Synopsis: {film.synopsis}</p>
+
+				{film.evaluations.map(e => (
+					<Comment evaluation={e} />
+				))}
+
+				<hr />
+			</div>
+		</div>
+	);
+};
+
+const Comment = props => {
+	const { evaluation } = props;
+
+	return (
+		<div className="uk-section uk-comment-meta uk-section-xsmall uk-section-secondary uk-light uk-margin-large-left">
+			<hr className="uk-divider-small" />
+			<div className="uk-container">
+				<h4>{evaluation.evaluator}</h4>
+				<p>Score: {evaluation.score}</p>
+				<p>Comment: {evaluation.comment}</p>
 			</div>
 		</div>
 	);
